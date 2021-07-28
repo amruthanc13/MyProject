bcfc413d6b72a5c503cd5ca2f8913b5c01ee9ddc
amruthanc13 <amruthanarendranc@gmail.com>
2021-07-28 09:59:06 +0200

    Added insurance and invoicing features

--- addons_bp/pharmacy/models/__init__.py
+++ addons_bp/pharmacy/models/__init__.py
@@ -3,3 +3,4 @@ from . import prescriptions
 from . import clinical_history
 from . import disease
 from . import medicine
+from . import insurance
--- addons_bp/pharmacy/models/prescriptions.py
+++ addons_bp/pharmacy/models/prescriptions.py
@@ -10,9 +10,18 @@ class SaleOrderInherit(models.Model):
 
     def action_confirm(self):
         super(SaleOrderInherit, self).action_confirm()
-        if self.state == 'sale' and self.diseases.exists():
+        if self.state == 'sale' and self.disease_ids.exists():
             vals = {'name': self.partner_id.id,
                     'date': self.date_order,
-                    'diseases': [(6, 0, self.diseases.ids)]}
+                    'disease_ids': [(6, 0, self.disease_ids.ids)]}
             self.env['pharmacy.clinical.history'].create(vals)
         return True
+
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrderInherit, self)._prepare_invoice()
+        partner = self.env['res.partner'].browse(
+            invoice_vals.get('partner_id'))
+        if partner.insurance_company_id.exists():
+            invoice_vals['partner_id'] = partner.insurance_company_id.address_get(['contact'])[
+                'contact']
+        return invoice_vals
